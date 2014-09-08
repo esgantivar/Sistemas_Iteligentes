ad21c7d6138d3f7bc0708bdff07b248156ebd524
cgalvist <cgalvist@gmail.com>
2014-09-08 13:33:34 -0500

    creado el paquete TeseoSimpleQuantalGraph que permite ejecutar el agente en el laberinto, ejemplos de laberintos agregados

--- src/unalcol/agents/examples/labyrinth/teseo/simple/TeseoSimpleQuantalGraph.java
+++ src/unalcol/agents/examples/labyrinth/teseo/simple/TeseoSimpleQuantalGraph.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package unalcol.agents.examples.labyrinth.teseo.simple;
 
 import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
@@ -11,10 +5,7 @@ import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseMultigraph;
 import java.util.ArrayList;
 import java.util.List;
-/**
- *
- * @author Sneyder G
- */
+
 public class TeseoSimpleQuantalGraph extends SimpleTeseoAgentProgram {
 
     @Override
