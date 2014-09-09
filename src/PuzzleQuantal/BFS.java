679ab7752764ff9683395859dc5f91ca4fa3a824
cgalvist <cgalvist@gmail.com>
2014-09-09 10:20:43 -0500

    bfs com problema de comparación de tableros ya recorridos

--- src/PuzzleQuantal/BFS.java
+++ src/PuzzleQuantal/BFS.java
@@ -1,27 +1,103 @@
 package PuzzleQuantal;
 
-//import java.util.ArrayList;
+import static PuzzleQuantal.Puzzle.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class BFS {
+
+    private static final ArrayList<int[][]> estadosRecorridos = new ArrayList<>();
+    private static Queue<int[][]> opciones = new LinkedList<>();
+    private static int[][] tablero = getTablero();
+
+    public static int[][] recorridoBFS() {
+
+//        tablero = getTablero();
+        estadosRecorridos.add(deepCopyIntMatrix(tablero));
+        int[] coordenadas;
+        int filaVacio;
+        int columnaVacio;
+        int[][] tableroTemporal;
+        while (!estaOrdenado(tablero)) {
+            coordenadas = buscarVacio();
+            filaVacio = coordenadas[0];
+            columnaVacio = coordenadas[1];
+            if (coordenadaValida(filaVacio + 1, columnaVacio)) {
+                tableroTemporal = tableroTemporal(filaVacio, columnaVacio, filaVacio + 1, columnaVacio);
+                if (!estadosRecorridos.contains(deepCopyIntMatrix(tableroTemporal))) {
+                    opciones.add(deepCopyIntMatrix(tableroTemporal));
+                }
+            }
+            if (coordenadaValida(filaVacio, columnaVacio + 1)) {
+                tableroTemporal = tableroTemporal(filaVacio, columnaVacio, filaVacio, columnaVacio + 1);
+                if (!estadosRecorridos.contains(deepCopyIntMatrix(tableroTemporal))) {
+                    opciones.add(deepCopyIntMatrix(tableroTemporal));
+                }
+            }
+            if (coordenadaValida(filaVacio - 1, columnaVacio)) {
+                tableroTemporal = tableroTemporal(filaVacio, columnaVacio, filaVacio - 1, columnaVacio);
+                if (!estadosRecorridos.contains(deepCopyIntMatrix(tableroTemporal))) {
+                    opciones.add(deepCopyIntMatrix(tableroTemporal));
+                }
+            }
+            if (coordenadaValida(filaVacio, columnaVacio - 1)) {
+                tableroTemporal = tableroTemporal(filaVacio, columnaVacio, filaVacio, columnaVacio - 1);
+                if (!estadosRecorridos.contains(deepCopyIntMatrix(tableroTemporal))) {
+                    opciones.add(deepCopyIntMatrix(tableroTemporal));
+                }
+            }
+
+            tablero = deepCopyIntMatrix(opciones.poll());
+            imprimirTablero(tablero);
+            estadosRecorridos.add(deepCopyIntMatrix(tablero));
+        }
+
+        return tablero;
+    }
     
-//    private boolean[] visitiadoAnchura = new boolean[5];
-//    
-//    public ArrayList<Integer> recorridoAnchura(int nodoI) {
-//        ArrayList<Integer> recorridos = new ArrayList<Integer>();
-//        visitiadoAnchura[nodoI] = true;
-//        ArrayList<Integer> cola = new ArrayList<Integer>();
-//        recorridos.add(nodoI);
-//        cola.add(nodoI);
-//        while (!cola.isEmpty()) {
-//            int j = cola.remove(0); 
-//                    for (int i = 0; i < g.length; i++) {
-//                        if (g[j][i] == 1 && !visitiadoAnchura[i]) {
-//                            cola.add(i);
-//                            recorridos.add(i);
-//                            visitiadoAnchura[i] = true;
-//                        }
-//                    }
-//                }
-//                return recorridos;
-//     }
+    private static boolean compararArray(ArrayList<int[][]> a1,int[][] a2){
+        for (int[][] h : a1) {
+            
+        }
+        return true;
+    }
+
+    private static int[] buscarVacio() {
+        int[] resultado = new int[2];
+        for (int fil = 0; fil < tablero.length; fil++) {
+            for (int col = 0; col < tablero[fil].length; col++) {
+                if (tablero[fil][col] == 0) {
+                    resultado[0] = fil;
+                    resultado[1] = col;
+                    return resultado;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static boolean coordenadaValida(int fila, int columna) {
+        return (fila >= 0 && columna >= 0 && fila <= 2 && columna <= 2);
+    }
+
+    private static int[][] tableroTemporal(int filOrigen, int colOrigen, int filDestino, int colDestino) {
+        int[][] tableroTemporal = deepCopyIntMatrix(tablero);
+        tableroTemporal[filOrigen][colOrigen] = tableroTemporal[filDestino][colDestino];
+        tableroTemporal[filDestino][colDestino] = 0;
+        return tableroTemporal;
+    }
+    
+    //clonar arreglo 2 dimensiones (Gracias Adam)
+    public static int[][] deepCopyIntMatrix(int[][] input) {
+        if (input == null) {
+            return null;
+        }
+        int[][] result = new int[input.length][];
+        for (int r = 0; r < input.length; r++) {
+            result[r] = input[r].clone();
+        }
+        return result;
+    }
 }
--- src/PuzzleQuantal/Puzzle.java
+++ src/PuzzleQuantal/Puzzle.java
@@ -1,22 +1,32 @@
 package PuzzleQuantal;
 
+import static PuzzleQuantal.BFS.*;
+import java.util.Arrays;
+
 //problema  8-puzzle
+
 public class Puzzle {
 
-//    private int[][] tablero = new int[2][2];
-    private static int[][] tablero = {{8, 4, 6}, {0, 2, 7}, {5, 1, 3}};
+//    private static int[][] tablero = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+    private static int[][] tablero = {{8, 4, 6}, {2, 0, 7}, {5, 1, 3}};
     private static final int[][] ordenado = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+//    private static int tablero = 846207513;
+//    private static final int ordenado = 123456780;
 
+    public static int[][] getTablero() {
+        return tablero;
+    }
+    
     public static void main(String[] args) {
         //tablero inicial
         System.out.println("tablero inicial:");
-        imprimirTablero();
+        imprimirTablero(tablero);
 
         System.out.println("tablero ordenado por BFS");
-        
+        imprimirTablero(recorridoBFS());
     }
-
-    public static void imprimirTablero() {
+    
+    public static void imprimirTablero(int[][] tablero) {
         for (int fil = 0; fil < tablero.length; fil++) {
             System.out.println("---------");
             for (int col = 0; col < tablero[fil].length; col++) {
@@ -27,8 +37,15 @@ public class Puzzle {
         System.out.println("---------");
     }
 
-    public static boolean estaOrdenado() {
-        return (tablero == ordenado);
+    //mejorar esta funcion para que imprima el tablero
+//    public static void imprimirTablero(int tablero) {
+//        System.out.println(tablero);
+//    }
+    
+    public static boolean estaOrdenado(int[][] tablero) {
+        return (Arrays.deepEquals(tablero, ordenado));
     }
-
+//    public static boolean estaOrdenado(int tablero) {
+//        return (tablero == ordenado);
+//    }
 }
